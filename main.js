5389516129dc3727a2da68c52413e1d69bef8ae4
fogaoquatrobocas <133917201+fogaoquatrobocas@users.noreply.github.com>
2024-11-01 08:44:52 -0300

    xx

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function criaCartao(Categoria,Pergunta,Resposta){
     let container = document.getElementById('container');
     let cartao = document.createElement('articicle');
     cartao.className = 'cartao';
-    cartao.innerHTML =  `
+    cartao.innerHTML = `
       <div class="conteudo-cartao">
       <h3>${Categoria}</h3>
     <div class="pergunta-cartao">
@@ -13,7 +13,9 @@ function criaCartao(Categoria,Pergunta,Resposta){
       </div>
   </div>
      `
+
      let RespostaEstavisivel = false;
+
      function viracartao(){
       RespostaEstavisivel = !RespostaEstavisivel;
       cartao.classList.toggle('active', RespostaEstavisivel);
